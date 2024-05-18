9befeedb5b2d36d2f9389ff2ae1fc944c72b2725
Vitaly Kuprin <vitas_s@inbox.lv>
2024-05-18 23:22:23 +0200

    chore: fix ts types

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GildedRoseComponent from "./components/GildedRose.tsx";
+import GildedRoseComponent from "./components/GildedRose";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
